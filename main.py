7cf028e2f8b1d41df51790838bf245cc1148688c
Rohan Jangotra <rohan@Rohans-MacBook-Pro.local>
2023-10-24 19:27:39 +0530

    base test url

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import time
 
 from urls.llm import llm_bp
 from urls.customLLM import custom_llm_bp
@@ -10,6 +11,10 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.register_blueprint(llm_bp, url_prefix='/llm')
 app.register_blueprint(custom_llm_bp, url_prefix='/custom-llm')
+
+@app.route('/')
+def index():
+    return {"success": True, "time": int(time.time()*1000)}
 
 if __name__ == "__main__":
     app.run(debug=True)
